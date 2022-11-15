47559cc546aabedca4eb8ca2e9fc0457c168c0b8
Guest User <Guest@Mac-mini-4.local>
2022-11-15 11:46:24 +0100

    setup

---
+++ EthWalletManagerApp/EthWalletManagerApp/EthWalletManagerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EthWalletManagerAppApp.swift
+//  EthWalletManagerApp
+//
+//  Created by Guest User on 15/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct EthWalletManagerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
